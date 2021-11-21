0f2dd2e942069b7d15ea8e00d2800936523a8998
Thomas Ballinger <thomasballinger@gmail.com>
2021-11-21 08:12:16 -0800

    bump version to 0.1.8

--- observable_jupyter_widget/_version.py
+++ observable_jupyter_widget/_version.py
@@ -4,5 +4,5 @@
 # Copyright (c) Thomas Ballinger.
 # Distributed under the terms of the Modified BSD License.
 
-version_info = (0, 1, 7, 'dev')
+version_info = (0, 1, 8, 'dev')
 __version__ = ".".join(map(str, version_info))
